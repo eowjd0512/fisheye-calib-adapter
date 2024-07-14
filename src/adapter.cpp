33cba1be7b28764a219bc8c3b42dfdc522699909
Sangjun Lee <eowjd0512@users.noreply.github.com>
2024-07-14 14:51:54 +0900

    reformulate PSNR SSIM

--- src/adapter.cpp
+++ src/adapter.cpp
@@ -90,9 +90,9 @@ void Adapter::show_image(const std::string & result_path)
   cv::imshow("Input model's projection", input_model_image);
   cv::imshow("Output model's projection", input_output_model_image);
 
-  // cv::imwrite(result_path + "/original_image.png", original_image);
-  // cv::imwrite(result_path + "/input_model_image.png", input_model_image);
-  // cv::imwrite(result_path + "/input_to_output_model_image.png", input_output_model_image);
+  cv::imwrite(result_path + "/original_image.png", original_image);
+  cv::imwrite(result_path + "/input_model_image.png", input_model_image);
+  cv::imwrite(result_path + "/input_to_output_model_image.png", input_output_model_image);
   cv::waitKey(0);
 }
 void Adapter::prepare_data(
@@ -157,54 +157,84 @@ void Adapter::evaluate_reprojection_error()
 
 double Adapter::calculate_psnr(const cv::Mat & img1, const cv::Mat & img2)
 {
-  return cv::PSNR(img1, img2);
+  cv::Mat gray1, gray2;
+  cv::cvtColor(img1, gray1, cv::COLOR_BGR2GRAY);
+  cv::cvtColor(img2, gray2, cv::COLOR_BGR2GRAY);
+
+  cv::Mat mask = (gray1 != 0) & (gray2 != 0);
+
+  cv::Mat s1;
+
+  cv::absdiff(img1, img2, s1);
+  s1.convertTo(s1, CV_32F);
+
+  cv::Mat s1_masked;
+  s1.copyTo(s1_masked, mask);
+
+  cv::Mat s1_squared = s1_masked.mul(s1_masked);
+
+  cv::Scalar s = cv::sum(s1_squared);
+  double sse = s.val[0];
+
+  if (sse <= 1e-10) {
+    return 0;
+  } else {
+    double mse = sse / cv::countNonZero(mask);
+    double psnr = 10.0 * log10((255 * 255) / mse);
+    return psnr;
+  }
 }
 
 double Adapter::calculate_ssim(const cv::Mat & img1, const cv::Mat & img2)
 {
   const double C1 = 6.5025, C2 = 58.5225;
-  int d = CV_32F;
+  cv::Mat mask = (img1 != 0) & (img2 != 0);
 
-  cv::Mat I1, I2;
-  img1.convertTo(I1, d);
-  img2.convertTo(I2, d);
+    cv::Mat I1, I2;
+    img1.convertTo(I1, CV_32F);
+    img2.convertTo(I2, CV_32F);
 
-  cv::Mat I1_2 = I1.mul(I1);   // I1^2
-  cv::Mat I2_2 = I2.mul(I2);   // I2^2
-  cv::Mat I1_I2 = I1.mul(I2);  // I1 * I2
+    cv::Mat I1_masked, I2_masked;
+    I1.copyTo(I1_masked, mask);
+    I2.copyTo(I2_masked, mask);
 
-  cv::Mat mu1, mu2;
-  cv::GaussianBlur(I1, mu1, cv::Size(11, 11), 1.5);
-  cv::GaussianBlur(I2, mu2, cv::Size(11, 11), 1.5);
+    cv::Mat I1_2 = I1_masked.mul(I1_masked);
+    cv::Mat I2_2 = I2_masked.mul(I2_masked);
+    cv::Mat I1_I2 = I1_masked.mul(I2_masked);
 
-  cv::Mat mu1_2 = mu1.mul(mu1);
-  cv::Mat mu2_2 = mu2.mul(mu2);
-  cv::Mat mu1_mu2 = mu1.mul(mu2);
+    cv::Mat mu1, mu2;
+    cv::GaussianBlur(I1_masked, mu1, cv::Size(11, 11), 1.5);
+    cv::GaussianBlur(I2_masked, mu2, cv::Size(11, 11), 1.5);
 
-  cv::Mat sigma1_2, sigma2_2, sigma12;
+    cv::Mat mu1_2 = mu1.mul(mu1);
+    cv::Mat mu2_2 = mu2.mul(mu2);
+    cv::Mat mu1_mu2 = mu1.mul(mu2);
 
-  cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
-  sigma1_2 -= mu1_2;
+    cv::Mat sigma1_2, sigma2_2, sigma12;
 
-  cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
-  sigma2_2 -= mu2_2;
+    cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+    sigma1_2 -= mu1_2;
 
-  cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
-  sigma12 -= mu1_mu2;
+    cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+    sigma2_2 -= mu2_2;
 
-  cv::Mat t1, t2, t3;
-  t1 = 2.0 * mu1_mu2 + C1;
-  t2 = 2.0 * sigma12 + C2;
-  t3 = t1.mul(t2);  // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+    cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+    sigma12 -= mu1_mu2;
 
-  t1 = mu1_2 + mu2_2 + C1;
-  t2 = sigma1_2 + sigma2_2 + C2;
-  t1 = t1.mul(t2);  // t1 = ((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+    cv::Mat t1, t2, t3;
+    t1 = 2.0 * mu1_mu2 + C1;
+    t2 = 2.0 * sigma12 + C2;
+    t3 = t1.mul(t2);
 
-  cv::Mat ssim_map;
-  cv::divide(t3, t1, ssim_map);  // ssim_map = t3./t1;
-  cv::Scalar mssim = cv::mean(ssim_map);
-  return mssim.val[0];
+    t1 = mu1_2 + mu2_2 + C1;
+    t2 = sigma1_2 + sigma2_2 + C2;
+    t1 = t1.mul(t2);
+
+    cv::Mat ssim_map;
+    cv::divide(t3, t1, ssim_map);
+
+    cv::Scalar mssim = cv::mean(ssim_map);
+    return mssim.val[0];
 }
 
 void Adapter::set_image(const std::string & image_path)
