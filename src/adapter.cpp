b836a5dd90c54ec5a7af02f2272e560a9957c228
Sangjun Lee <eowjd0512@users.noreply.github.com>
2024-08-02 22:56:31 +0900

    Applied clang format

--- src/adapter.cpp
+++ src/adapter.cpp
@@ -190,51 +190,51 @@ double Adapter::calculate_ssim(const cv::Mat & img1, const cv::Mat & img2)
   const double C1 = 6.5025, C2 = 58.5225;
   cv::Mat mask = (img1 != 0) & (img2 != 0);
 
-    cv::Mat I1, I2;
-    img1.convertTo(I1, CV_32F);
-    img2.convertTo(I2, CV_32F);
+  cv::Mat I1, I2;
+  img1.convertTo(I1, CV_32F);
+  img2.convertTo(I2, CV_32F);
 
-    cv::Mat I1_masked, I2_masked;
-    I1.copyTo(I1_masked, mask);
-    I2.copyTo(I2_masked, mask);
+  cv::Mat I1_masked, I2_masked;
+  I1.copyTo(I1_masked, mask);
+  I2.copyTo(I2_masked, mask);
 
-    cv::Mat I1_2 = I1_masked.mul(I1_masked);
-    cv::Mat I2_2 = I2_masked.mul(I2_masked);
-    cv::Mat I1_I2 = I1_masked.mul(I2_masked);
+  cv::Mat I1_2 = I1_masked.mul(I1_masked);
+  cv::Mat I2_2 = I2_masked.mul(I2_masked);
+  cv::Mat I1_I2 = I1_masked.mul(I2_masked);
 
-    cv::Mat mu1, mu2;
-    cv::GaussianBlur(I1_masked, mu1, cv::Size(11, 11), 1.5);
-    cv::GaussianBlur(I2_masked, mu2, cv::Size(11, 11), 1.5);
+  cv::Mat mu1, mu2;
+  cv::GaussianBlur(I1_masked, mu1, cv::Size(11, 11), 1.5);
+  cv::GaussianBlur(I2_masked, mu2, cv::Size(11, 11), 1.5);
 
-    cv::Mat mu1_2 = mu1.mul(mu1);
-    cv::Mat mu2_2 = mu2.mul(mu2);
-    cv::Mat mu1_mu2 = mu1.mul(mu2);
+  cv::Mat mu1_2 = mu1.mul(mu1);
+  cv::Mat mu2_2 = mu2.mul(mu2);
+  cv::Mat mu1_mu2 = mu1.mul(mu2);
 
-    cv::Mat sigma1_2, sigma2_2, sigma12;
+  cv::Mat sigma1_2, sigma2_2, sigma12;
 
-    cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
-    sigma1_2 -= mu1_2;
+  cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+  sigma1_2 -= mu1_2;
 
-    cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
-    sigma2_2 -= mu2_2;
+  cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+  sigma2_2 -= mu2_2;
 
-    cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
-    sigma12 -= mu1_mu2;
+  cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+  sigma12 -= mu1_mu2;
 
-    cv::Mat t1, t2, t3;
-    t1 = 2.0 * mu1_mu2 + C1;
-    t2 = 2.0 * sigma12 + C2;
-    t3 = t1.mul(t2);
+  cv::Mat t1, t2, t3;
+  t1 = 2.0 * mu1_mu2 + C1;
+  t2 = 2.0 * sigma12 + C2;
+  t3 = t1.mul(t2);
 
-    t1 = mu1_2 + mu2_2 + C1;
-    t2 = sigma1_2 + sigma2_2 + C2;
-    t1 = t1.mul(t2);
+  t1 = mu1_2 + mu2_2 + C1;
+  t2 = sigma1_2 + sigma2_2 + C2;
+  t1 = t1.mul(t2);
 
-    cv::Mat ssim_map;
-    cv::divide(t3, t1, ssim_map);
+  cv::Mat ssim_map;
+  cv::divide(t3, t1, ssim_map);
 
-    cv::Scalar mssim = cv::mean(ssim_map);
-    return mssim.val[0];
+  cv::Scalar mssim = cv::mean(ssim_map);
+  return mssim.val[0];
 }
 
 void Adapter::set_image(const std::string & image_path)
